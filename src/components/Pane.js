c33d6910c8fab65bef20c608f48beee8ceb4f3a7
Manuel Venegas <mengasis@outlook.com>
2018-11-16 13:59:56 -0300

    Created Pane component

---
+++ src/components/Pane.js
@@ -0,0 +1,10 @@
+import styled from 'react-emotion'
+
+export const Pane = styled('div')`
+	background: #fff;
+	border: 1px solid #c5cad4;
+	border-radius: 4px;
+	padding: 32px;
+	overflow-y: auto;
+	width: 400px;
+`
