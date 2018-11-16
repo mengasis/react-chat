cd6d1c394c56409a0a88513fa996cc7424c09d23
Manuel Venegas <mengasis@outlook.com>
2018-11-16 14:21:32 -0300

    Created Button component

---
+++ src/components/Button.js
@@ -0,0 +1,12 @@
+import styled from 'react-emotion'
+
+export const Button = styled('button')`
+	background: #23cca4;
+	border: none;
+	border-radius: 4px;
+	color: #fff;
+	font-size: 16px;
+	font-weight: bold;
+	padding: 8px 10px;
+	text-transform: uppercase;
+`
