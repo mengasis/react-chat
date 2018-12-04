bbf8c44dcdf04772e64da04d34ef367709ca6872
Manuel Venegas <mengasis@outlook.com>
2018-12-04 01:47:56 -0300

    Updates

---
+++ precache-manifest.d1ffdd061d91f773c0a7069676a97e02.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "c79d0569fe4b36a81978",
+    "url": "/react-chat/static/js/runtime~main.c79d0569.js"
+  },
+  {
+    "revision": "7083b86ee8c0e39c92e7",
+    "url": "/react-chat/static/js/main.7083b86e.chunk.js"
+  },
+  {
+    "revision": "09821eb426e2b2ddcf59",
+    "url": "/react-chat/static/js/1.09821eb4.chunk.js"
+  },
+  {
+    "revision": "0d82f37a85379aa35527988ab555cd64",
+    "url": "/react-chat/index.html"
+  }
+];
